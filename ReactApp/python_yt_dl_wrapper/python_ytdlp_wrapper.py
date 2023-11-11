1e6498b250b588cae7447bc60d33a83a3986c066
Eddie <ehines@umass.edu>
2023-11-11 13:23:35 -0500

    Trying to deal with the stupid format of this file

--- ReactApp/python_yt_dl_wrapper/python_ytdlp_wrapper.py
+++ ReactApp/python_yt_dl_wrapper/python_ytdlp_wrapper.py
@@ -39,37 +39,48 @@ class wrapper():
     def download(self, url, *options):
         args = ["yt-dlp", url, *options]
         spawn(self.full_path, *args)
-    def decode_format(self, fm): # return a new format object with decoded format
+    def decode_format_header(self, header, exclude=""):
+         last_space = True
+         result = []
+         header = header.replace("|", "")
+         header = header.replace(exclude, "")
+         for i,c in enumerate(header):
+            if c != " " and last_space:
+                 last_space = False
+                 result.append(i)
+            elif c == " ":
+                 last_space = True
+         return result, header
+    def decode_format(self, fm, header_slices, header): # return a new format object with decoded format
         fm = fm.replace("|", "")
-        fm = fm.replace("only", "")
-        fm = fm.replace("~", "")
-        fm = fm.replace("≈", "")
-        fm = fm.split()
-        fid = fm[0]
-        container = fm[1]
-        resolution = fm[2]
-        if(fm[2] != 'audio'):
-            fps=fm[3]
-            protocol=fm[4]
-            
-        else:
-            fps=-1
-            protocol=fm[3]
+        result = {}
+        for i, elem in enumerate(header_slices):
+              if(i+1 >= len(header_slices)):
+                   result[header[elem:]] = fm[elem:]
+              else:
+                   result[header[elem:header_slices[i+1]]] = fm[elem:header_slices[i+1]]        
+        return result
     def get_formats(self, url):
         args = ["yt-dlp", url, "-F"]
         spawn_out_to_file("formats.txt", self.full_path, *args)
         with open("formats.txt", "r") as formats:
+             header_slices = []
+             header = ""
              start = False
+             fms = []
              for line in formats:
                   if start:
-                       self.decode_format(line)
+                       fms.append(self.decode_format(line, header_slices, header))
+                  if "ID" in line:
+                       header_slices, header = self.decode_format_header(line, exclude="MORE INFO\n")
                   if "-" not in line:
                        continue
                   elif "-" in line:
                        start = True
                        continue
-
+             print([fms[i]['FILESIZE   '] for i in range(len(fms))])
 if __name__ == "__main__":
     a_wrapper = wrapper()
     a_wrapper.check_version()
     a_wrapper.get_formats("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    
