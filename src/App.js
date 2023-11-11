ccbb29b80ca54a8ba7e3d0ec4478d8702b3e04c4
Eddie <ehines@umass.edu>
2023-11-10 23:41:51 -0500

    Broke ground on the frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please enter the URL to a YouTube video that you want to download.
         </p>
         <a
           className="App-link"
